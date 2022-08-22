ea0fd2efdcfa6d2bbe2995b26551614525470d8a
John Evander Castillo <john.e.castillo@accenture.com>
2022-08-22 18:25:14 +0800

    Commit at end of Part 3

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,15 +1,22 @@
 const resolvers = {
     Query: {
         // returns an array of Track that will be used to populate
-        // the hompage grid of our web client 
+        // the homepage grid of our web client 
         tracksForHome: (_, __, {dataSources}) => {
           return dataSources.trackAPI.getTracksForHome();
-        }
+        },
+        // get a single track by id, for the Track page
+        track: (_, {id}, {dataSources}) => {
+          return dataSources.trackAPI.getTrack(id);
+        },
     },
     Track: {    
         author: ({authorId}, _, {dataSources}) => {
             return dataSources.trackAPI.getAuthor(authorId);
         },
+        modules: ({id}, _, {dataSources}) => {
+            return dataSources.trackAPI.getTrackModules(id);
+        }
     },
 };
 
